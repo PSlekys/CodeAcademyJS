ec66c5a8eee450570bcce76100907ad2de82799e
PSlekys <petras@mozilla.lt>
2021-02-23 20:53:54 +0200

    task 2 and 3 complete

---
+++ demo2/index.js
@@ -0,0 +1,8 @@
+const name1 = "Petras";
+const name2 = "Jonas";
+
+if (name1.length + name2.length > 10) {
+  alert(name1.length > name2.length ? name1 : name2);
+} else {
+  alert("Abu vardai gražūs");
+}
---
+++ demo3/index.js
@@ -0,0 +1,3 @@
+const n1 = 5;
+
+n1 % 2 === 0 ? alert("yay") : alert("pabandyk " + (n1 - 1));
